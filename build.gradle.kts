ba0f5ffe9fc48d1e9e3d27ac1f095278aa94aa18
PARK SUNG DONG <oraedoa@gmail.com>
2023-03-04 15:30:22 +0900

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.9"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.jpa") version "1.6.21"
+    val kotlinVersion = System.getProperty("version.kotlinVersion")
+    val springBootVersion = System.getProperty("version.springBootVersion")
+    val springBootManagementVersion = System.getProperty("version.springDependencyManagementVersion")
+    val klintVersion = System.getProperty("version.ktlintVersion")
+
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version springBootManagementVersion
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+
+    // kotlin lint
+    id("org.jlleitschuh.gradle.ktlint") version klintVersion
 }
 
 group = "com.github"
@@ -17,21 +27,39 @@ repositories {
 }
 
 extra["testcontainersVersion"] = "1.17.6"
+val kotestVersion: String = System.getProperty("version.kotestVersion")
+val mockkVersion: String = System.getProperty("version.mocckVersion")
+val springmockkVersion: String = System.getProperty("version.springmockkVersion")
+val testcontainersVersion = System.getProperty("version.testcontainerVersion")
+val springStatemachineVersion = System.getProperty("version.springStatemachineVersion")
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // state-machine
+    implementation("org.springframework.statemachine:spring-statemachine-starter")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
+
+    // mock & kotest
+    testImplementation("io.mockk:mockk:${mockkVersion}")
+    testImplementation("io.kotest:kotest-assertions-core:${kotestVersion}")
+    testImplementation("com.ninja-squad:springmockk:${springmockkVersion}")
 }
 
 dependencyManagement {
     imports {
-        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
+        mavenBom("org.testcontainers:testcontainers-bom:${testcontainersVersion}")
+        mavenBom("org.springframework.statemachine:spring-statemachine-bom:${springStatemachineVersion}")
     }
 }
 
